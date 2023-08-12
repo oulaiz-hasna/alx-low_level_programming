382d26f78e8a5817a460ebac1885305ce10c45ae
oulaiz-hasna <hasnaoulaiz97@gmail.com>
2023-08-11 17:09:24 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Function that serves as the Entry point of c programs.
+ *
+* Return: (0) - Successful termination of a program.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
