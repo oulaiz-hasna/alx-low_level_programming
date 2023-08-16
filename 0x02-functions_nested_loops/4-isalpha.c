01260b9d9cafdc61713fec57e24d8c63ddc768ce
oulaiz-hasna <hasnaoulaiz97@gmail.com>
2023-08-16 09:07:50 -0700

    [Adding] 4-isalpha.c file, [UPDATING] main.h file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if input is a letter: both lower or upper case.
+ *
+ * @c: the char is ASCII code
+ *
+ * Return: returns 1 if 'c' is a letter, 0 otherwise.
+*/
+int _isalpah(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	return (0);
+}
