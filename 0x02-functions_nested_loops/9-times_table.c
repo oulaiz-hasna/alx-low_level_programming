95870da8243a7abce8c6442df091ac90b06f89a0
oulaiz-hasna <hasnaoulaiz97@gmail.com>
2023-08-20 07:48:42 -0700

    Little fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,19 +14,19 @@ void times_table(void)
 		for (b = 1; b <= 9; b++)
 		{
 			op = a * b;
+
 			_putchar(44);
 			_putchar(32);
+
 			if (op <= 9)
 			{
-				_putchar(32);
+				_putchar(' ');
 				_putchar(op + 48);
 			}
 			else
-			{
 				_putchar((op / 10) + 48);
-				_putchar((op % 10) + 48);
-			}
+			_putchar((op % 10) + 48);
 		}
 		_putchar('\n');
 	}
-}	
+}
