4f6294412ac972d95c082be6d71b13e7b84a2ea3
oulaiz-hasna <hasnaoulaiz97@gmail.com>
2023-08-22 15:03:55 -0700

    [Adding] 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * _strlen - returns the length of a string
+ *
+ * @s: string parametre input
+ *
+ * Return: Length of a string
+*/
+
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; *s != '\0'; s++)
+		++counter;
+	return (counter);
+}
