80449f97b64b577acf4c25778299778da0ebc396
oulaiz-hasna <hasnaoulaiz97@gmail.com>
2023-08-22 16:04:59 -0700

    [Fixed] betty documentation style

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
-/*
- * _strlen - returns the length of a string
- *
- * @s: string parametre input
- *
- * Return: Length of a string
-*/
+/**
+ * _strlen - returns the length of a string.
+ * @s: input string.
+ * Return: length of a string.
+ */
 int _strlen(char *s)
 {
 	int counter;
