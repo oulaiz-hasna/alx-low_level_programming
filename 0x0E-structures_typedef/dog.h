d9c4ae9fde26ae4808d7baec1d9f6460d06f3d5f
oulaiz-hasna <hasnaoulaiz97@gmail.com>
2023-09-11 14:43:03 -0700

    A DogStruct: The PawPower in a Digital World

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info in a struc form.
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Description: Meet the 'Pup-Struct': Your digital doggie data
+ * digs! It's tail-waggingly efficient.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
